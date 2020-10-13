0151fc52800c6671496c40337a76bdc01d87fa45
O.Bilenko <oleksandr.bilenko@shopgate.com>
2020-10-13 11:47:26 +0200

    CCP-2220 Add Tabs component

---
+++ frontend/components/Tabs/index.jsx
@@ -0,0 +1,96 @@
+import React, { useState, Fragment, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { css } from 'glamor';
+import { Grid } from '@shopgate/engage/components';
+import { themeConfig } from '@shopgate/engage';
+
+const styles = {
+  tabs: css({
+    borderBottom: `1px solid ${themeConfig.colors.shade3}`,
+    background: themeConfig.colors.light,
+    boxShadow: `0px 1rem 0.5rem ${themeConfig.colors.light}`,
+    marginBottom: '0.5rem',
+    paddingTop: '0.5rem',
+  }).toString(),
+  tabsSticky: css({
+    top: 0,
+    position: 'sticky',
+    zIndex: 5,
+  }).toString(),
+  tab: css({
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '0.5rem 0',
+    borderBottom: '3px solid transparent',
+  }).toString(),
+  tabActive: css({
+    borderBottom: `3px solid ${themeConfig.colors.primary}`,
+  }).toString(),
+};
+
+/**
+ * @returns {JSX}
+ */
+export const Tab = ({ title, active, onClick }) => (
+  <Grid.Item
+    grow={1}
+    className={`${styles.tab} ${active ? styles.tabActive : ''}`}
+    onClick={onClick}
+  >
+    {title}
+  </Grid.Item>
+);
+Tab.propTypes = {
+  title: PropTypes.string.isRequired,
+  active: PropTypes.bool,
+  onClick: PropTypes.func,
+};
+Tab.defaultProps = {
+  active: false,
+  onClick: () => {},
+};
+
+/**
+ * @returns {JSX}
+ */
+const Tabs = ({ sticky, children }) => {
+  const [activeTab, setActiveTab] = useState(() => {
+    const flatChildren = React.Children.toArray(children);
+    const active = flatChildren.find(c => c.props.active);
+    if (active) {
+      return active.props.title;
+    }
+    return flatChildren[0].props.title;
+  });
+
+  const content = useMemo(() => {
+    const flatChildren = React.Children.toArray(children);
+    const active = flatChildren.find(c => c.props.title === activeTab);
+    return active.props.children;
+  }, [activeTab, children]);
+
+  return (
+    <Fragment>
+      <Grid className={`${styles.tabs} ${sticky ? styles.tabsSticky : ''}`}>
+        {React.Children.map(children, child => (
+          React.cloneElement(child, {
+            active: activeTab === child.props.title,
+            onClick: () => setActiveTab(child.props.title),
+          })
+        ))}
+      </Grid>
+      {content}
+    </Fragment>
+  );
+};
+
+Tabs.propTypes = {
+  children: PropTypes.node.isRequired,
+  sticky: PropTypes.bool,
+};
+Tabs.defaultProps = {
+  sticky: true,
+};
+
+Tabs.Tab = Tab;
+export default Tabs;
--- frontend/pages/Coupons/index.jsx
+++ frontend/pages/Coupons/index.jsx
@@ -1,89 +1,31 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { css } from 'glamor';
-import {
-  useTheme, useNavigation, i18n, useRoute,
-} from '@shopgate/engage/core';
+import { i18n, useTheme } from '@shopgate/engage/core';
 import { Route } from '@shopgate/engage/components';
-import { Tabs, Tab, TabPanel } from '@shopgate/engage/components/Tabs';
-import I18n from '@shopgate/pwa-common/components/I18n';
-import { TabContext } from '@shopgate/engage/components/Tabs/TabContext';
-import { themeConfig } from '@shopgate/pwa-common/helpers/config';
-import { responsiveMediaQuery } from '@shopgate/engage/styles';
 import { LOYALTY_COUPONS_ROUTE } from '../../constants';
 import MyCoupons from './MyCoupons';
 import RedeemCoupons from './RedeemCoupons';
-
-const { variables } = themeConfig;
+import Tabs, { Tab } from '../../components/Tabs';
 
 const styles = {
   content: css({
 
   }),
-  coupons: css({
-    padding: '1rem',
-  }),
 };
 
-export const title = css({
-  fontSize: '1.5rem',
-  padding: variables.gap.big,
-}).toString();
-
-export const tabs = css({
-  width: '100%',
-  top: 0,
-  [responsiveMediaQuery('>xs', { webOnly: true })]: {
-    top: 64,
-  },
-  [responsiveMediaQuery('<=xs', { webOnly: true })]: {
-    top: 56,
-  },
-  position: 'sticky',
-  background: 'rgb(255, 255, 255)',
-  boxShadow: '2px 1px 6px rgba(0, 0, 0, 0.118), 2px 1px 4px rgba(0, 0, 0, 0.118)',
-  zIndex: 100,
-}).toString();
-
-export const tabPanel = css({
-  padding: variables.gap.small,
-}).toString();
-
-const TAB_COUPONS = 'coupons';
-const TAB_REDEEM = 'redeem';
-
 export const Coupons = () => {
-  const { replace } = useNavigation();
   const { View, AppBar } = useTheme();
-  const { params: { tab = TAB_COUPONS } } = useRoute();
-
   return (
     <View>
-      <AppBar title="ps_loyalty.coupons.title" />
-
-      <TabContext value={tab}>
-        <div
-          className={tabs}
-        >
-          <Tabs
-            indicatorColor="primary"
-            textColor="primary"
-            onChange={(event, newValue) => replace({ pathname: `${LOYALTY_COUPONS_ROUTE}/${newValue}` })}
-            aria-label="disabled tabs example"
-          >
-            <Tab value={TAB_COUPONS} label={i18n.text('#COUPONS')} />
-
-            <Tab value={TAB_REDEEM} label={i18n.text('#EINLÖSEN')} />
-          </Tabs>
-        </div>
-        <TabPanel className={tabPanel} value={TAB_COUPONS}>
+      <AppBar title="ps_loyalty.coupon.title" />
+      <Tabs>
+        <Tab title={i18n.text('#COUPONS')} key="coupons">
           <MyCoupons />
-        </TabPanel>
-        <TabPanel className={tabPanel} value={TAB_REDEEM}>
+        </Tab>
+        <Tab title={i18n.text('#EINLÖSEN')} key="points">
           <RedeemCoupons />
-        </TabPanel>
-      </TabContext>
-
+        </Tab>
+      </Tabs>
     </View>
   );
 };
